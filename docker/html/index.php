c3dce96898841ed517d08e3d091e16ca27781885
Yiming-Cao <1192702@student.roc-nijmegen.nl>
2024-03-01 13:54:57 +0100

    1

---
+++ docker/html/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/themain.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <div class="header">
+            <div class="headerRight">
+                <div class="logo">
+                    <svg width="140" height="140">
+                        <circle cx="70" cy="70" r="60" stroke="none" stroke-width="3" fill="#F6D6C3" />
+                    </svg>
+                </div>
+            </div>
+            <div class="headerLeft">
+                <div class="menu">
+                    <h1>MENU</h1>
+                </div>
+                <div class="login">
+                    <h1>LOGIN</h1>
+                </div>
+                <div class="go">
+                    <svg width="82" height="10">
+                        <line x1="10" y1="5" x2="72" y2="5" stroke="black" stroke-width="2" />
+                    </svg>
+                    <svg width="82" height="10">
+                        <line x1="10" y1="4" x2="72" y2="4" stroke="black" stroke-width="2" />
+                    </svg>
+                    <svg width="82" height="10">
+                        <line x1="10" y1="4" x2="72" y2="4" stroke="black" stroke-width="2" />
+                    </svg>
+                </div>
+            </div>
+    </div>
+    <div class="mid">
+        <div class="vlak-1">
+            <div class="vlak-1-left">
+                <div class="vlak-1-text">
+                    <h2>
+                        Geniet van Aziatische smaken
+                    </h2>
+                </div>
+                <div class="vlak-1-bottom">
+
+                </div>
+            </div>
+            <div class="vlak-1-right">
+                <img src="image/Frame 7.png" alt="">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
